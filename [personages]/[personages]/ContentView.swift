46bcf2b80e0188d9f7a588f0e73ac730fe2ca84b
Karrie <karinad000@gmail.com>
2024-06-09 13:45:50 +0300

    Xcode project created

---
+++ [personages]/[personages]/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  [personages]
+//
+//  Created by Карина Дьячина on 9.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ [personages]/[personages]/_personages_App.swift
@@ -0,0 +1,17 @@
+//
+//  _personages_App.swift
+//  [personages]
+//
+//  Created by Карина Дьячина on 9.06.24.
+//
+
+import SwiftUI
+
+@main
+struct _personages_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
